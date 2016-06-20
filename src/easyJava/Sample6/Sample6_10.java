7ec21aed172e9196af9f30f3e29fe2a6b5944169
Masaki Tomizawa <thunderclap224@gmail.com>
2016-06-20 14:09:25 +0900

    途中だよ

---
+++ src/easyJava/Sample6/Sample6_10.java
@@ -0,0 +1,17 @@
+package easyJava.Sample6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Sample6_10 {
+	public static void main(String[] args) throws IOException{
+		System.out.println("成績を入力してください。");
+		
+		BufferedReader br =
+				new BufferedReader(new InputStreamReader(System.in));
+		
+		
+	}
+
+}
---
+++ src/easyJava/Sample6/Sample6_9.java
@@ -0,0 +1,23 @@
+package easyJava.Sample6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Sample6_9 {
+	public static void main(String[] args) throws IOException {
+		System.out.println("何番目でループを中止しますか？(1~10)");
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str = br.readLine();
+		int res = Integer.parseInt(str);
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i + "番目の処理です。");
+			if(i == res)
+				break;
+		}
+
+	}
+}
